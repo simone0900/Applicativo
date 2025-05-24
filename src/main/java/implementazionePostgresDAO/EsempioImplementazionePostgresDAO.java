caa39728191160669476dda98d67ab266d5dd750
PorfirioTramontana <PorfirioTramontana@users.noreply.github.com>
2025-05-24 14:11:44 +0200

    Added DAO ed EsempioImplementazione

---
+++ src/main/java/implementazionePostgresDAO/EsempioImplementazionePostgresDAO.java
@@ -0,0 +1,27 @@
+package implementazioniPostgresDAO;
+
+import Database.ConnessioneDatabase;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+public class EsempioImplementazionePostgresDAO implements EsempioDAO {
+
+	private Connection connection;
+
+	public EsempioImplementazionePostgresDAO() {
+		try {
+			connection = ConnessioneDatabase.getInstance().connection;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public void esempioQuery() {
+
+	}
+
+}
